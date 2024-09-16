b2bc60fb1a807499a8afc44abc00441af48077f5
vitoria-csantos <vitoria.santos659@etec.sp.gov.br>
2024-09-16 06:14:13 -0300

    Adicionando apenas os scripts porque o jogo inteiro era grande demais

---
+++ audioManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+ 
+  [SerializeField] AudioSource musicSorce;
+    [SerializeField] AudioSource musicSorce01;
+    [SerializeField] AudioSource musicSorce02;
+    [SerializeField] AudioSource musicSorce03;
+    [SerializeField] AudioSource musicSorce04;
+    [SerializeField] AudioSource musicSorce05;
+    [SerializeField] AudioSource musicSorce06;
+
+}
